a2a65eb1da0845a2810e719f60f051cb3f8f78a9
ignacio lizarazu aramayo <lizarazuaramayoignacio@gmail.com>
2023-06-29 16:56:13 -0400

    creacion home page y formulario con hooks

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import StoreProvider from './context/ContextProvider'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router/AppRouter'
 
+
 function App() {
 
   return (
--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
-
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { useStore } from "../../context/ContextProvider";
+import HomePage from '../pages/HomePage';
 import TemporaryDrawer from "../layout/AppLayout";
 import Incallojeta1 from "../components/IncaLlojNorte";
 import Incallojeta2 from "../components/IncaLlojSur";
@@ -13,41 +13,29 @@ import CajaFerroviaria from "../components/CajaFerroviaria";
 import IrpaviNorte from "../components/IrpaviNorte";
 import IrpaviSur from "../components/IrpaviSur";
 import LaPortada from "../components/LaPortada";
+import FormsPage from "../pages/FormsPage"; // Agrega la importación del componente FormsPage
 
 export const AppRoutes = () => {
   const { auth } = useStore();
-  return (
-  <Routes>
-    <Route path="/" element={
-    <TemporaryDrawer
-    />}>
-    <Route path="" >
-       
-      <Route 
-      path="incaNorte" element={<Incallojeta1/>} /> 
-       <Route 
-      path="incaSur" element={<Incallojeta2/>} /> 
-       <Route 
-      path="chasquipampa" element={<Chasquipampa/>} /> 
-       <Route 
-      path="achumani" element={<Achumani/>} /> 
-       <Route 
-      path="villaSalome" element={<VillaSalome/>} /> 
-       <Route 
-      path="integradora" element={<Integradora/>} /> 
-       <Route 
-      path="cajaFerroviaria" element={<CajaFerroviaria/>} /> 
-      <Route 
-      path="irpaviNorte" element={<IrpaviNorte/>} /> 
-      <Route 
-      path="irpaviSur" element={<IrpaviSur/>} />
-      <Route 
-      path="laPortada" element={<LaPortada/>} />  
-      </Route>
-      </Route>
-      
-      
-  </Routes>
-  )
-};
 
+  return (
+    <Routes>
+      <Route path="/" element={<TemporaryDrawer />}>
+        <Route path="">
+          <Route path="home" element={<HomePage />} />
+          <Route path="incaNorte" element={<Incallojeta1 />} />
+          <Route path="incaSur" element={<Incallojeta2 />} />
+          <Route path="chasquipampa" element={<Chasquipampa />} />
+          <Route path="achumani" element={<Achumani />} />
+          <Route path="villaSalome" element={<VillaSalome />} />
+          <Route path="integradora" element={<Integradora />} />
+          <Route path="cajaFerroviaria" element={<CajaFerroviaria />} />
+          <Route path="irpaviNorte" element={<IrpaviNorte />} />
+          <Route path="irpaviSur" element={<IrpaviSur />} />
+          <Route path="laPortada" element={<LaPortada />} />
+          <Route path="forms" element={<FormsPage />} /> {/* Agrega la ruta para el componente FormsPage */}
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
